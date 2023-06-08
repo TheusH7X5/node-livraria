fb7dfc720041cfd719be8d74120b30b07e2ab3f6
TheusH7X5 <matheush7x5@gmail.com>
2023-06-08 16:23:50 -0300

    ajuste

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
-import connectToMongoDB from "./config/dbConnect.js";
+import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
 import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
 import manipulador404 from "./middlewares/manipulador404.js";
 
-connectToMongoDB.on("error", console.log.bind(console, "Erro de conexão"));
-connectToMongoDB.once("open", () => {
+db.on("error", console.log.bind(console, "Erro de conexão"));
+db.once("open", () => {
   console.log("Conexão com o banco feita com sucesso");
 });
 
