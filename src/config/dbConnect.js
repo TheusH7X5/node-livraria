297d48149b9b98f548c2f43e4880f7f240dd31df
TheusH7X5 <matheush7x5@gmail.com>
2023-06-08 19:26:42 -0300

    ajust

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,16 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conectado ao banco de dados MongoDB");
+  })
+  .catch((error) => {
+    console.error("Erro ao conectar ao banco de dados:", error);
+  });
 
 let db = mongoose.connection;
 
